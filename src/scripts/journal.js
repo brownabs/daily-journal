f476617cf156b0cd6b3068d48540e349d05b2e80
Abbey Brown <abbeybrownsemail@gmail.com>
2019-01-22 14:58:27 -0600

    daily-journal-1-4

---
+++ src/scripts/journal.js
@@ -0,0 +1,25 @@
+fetch("http://localhost:3000/entries") //implementation of a promise //make the request
+    .then(entries => entries.json()) //taking a string and then turning into a js object//parsing the response
+    .then(parsedEntries => { //parsedEntries is the argument to the function //this function is anonymous/now we can do shit
+        parsedEntries.forEach(entries => {
+            const entryHTML = createEntryHTMLComponent(entries)
+            printEntry(entryHTML)
+
+        })
+    })
+
+    const createEntryHTMLComponent = completedEntry => {
+        return `
+    <article class="dailyEntryLog">
+        <section class="entryDate">${completedEntry.date}</section>
+        <section class="entryConcepts"> Concepts Covered: ${completedEntry.conceptsCovered}</section>
+        <section class="entryEntry">Entry: ${completedEntry.entry}</section>
+        <section class="entryMood"> Mood: ${completedEntry.mood}</section>
+    </div>
+    `
+    }
+
+const printEntry = entryHTML => {
+    document.querySelector(".entryLog").innerHTML += entryHTML
+}
+
